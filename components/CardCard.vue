ad6ae37edc2cb805ea7022de4830bc075a02e180
Dalton Dayton <daltondayton1@gmail.com>
2019-10-10 15:18:00 -0600

    add api service, add card to store, add card component, update collection to display card

---
+++ components/CardCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-card max-width="520" class="mx-auto">
+      <!-- <v-card-title>{{ card.name }}</v-card-title> -->
+      <v-card-text>
+        <img :src="card.image_uris.normal" />
+      </v-card-text>
+    </v-card>
+    {{ card }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardCard',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    card: Object
+  }
+}
+</script>
+
+<style scoped></style>
--- pages/collection.vue
+++ pages/collection.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
     <h1 class="text-center">Collection</h1>
+
+    <CardCard :card="cards" />
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+import CardCard from '@/components/CardCard.vue'
+
+export default {
+  components: {
+    CardCard
+  },
+  computed: mapState({
+    cards: (state) => state.cards.cards
+  }),
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('cards/fetchCards')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch cards at this time. Please try again.'
+      })
+    }
+  }
+}
 </script>
 
 <style scoped></style>
