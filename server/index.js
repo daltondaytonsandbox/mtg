3a64645a0051d21e388797ad39703910a77de324
Dalton Dayton <daltondayton1@gmail.com>
2019-10-10 18:18:50 -0600

    add Mongo / Mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-console */
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const mongoose = require('mongoose')
+
+// Mongoose Set Up
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  // Connected
+  // const kittySchema = new mongoose.Schema({
+  //   name: String
+  // })
+  // const Kitten = mongoose.model('Kitten', kittySchema)
+  // const silence = new Kitten({ name: 'Silence' })
+  // const fluffy = new Kitten({ name: 'fluffy' })
+  // fluffy.save(function(err, fluffy) {
+  //   if (err) return console.error(err)
+  // })
+})
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
