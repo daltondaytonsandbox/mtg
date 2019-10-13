ad3092676a2955de359e755b0a386bfe4d3c6245
Dalton Dayton <daltondayton1@gmail.com>
2019-10-12 19:20:54 -0600

    add partially functional server

--- server/index.js
+++ server/index.js
@@ -1,30 +1,20 @@
 /* eslint-disable no-console */
+const { Nuxt, Builder } = require('nuxt')
+const bodyParser = require('body-parser')
 const express = require('express')
 const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const mongoose = require('mongoose')
 
-// Mongoose Set Up
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
-  // Connected
-  // const kittySchema = new mongoose.Schema({
-  //   name: String
-  // })
-  // const Kitten = mongoose.model('Kitten', kittySchema)
-  // const silence = new Kitten({ name: 'Silence' })
-  // const fluffy = new Kitten({ name: 'fluffy' })
-  // fluffy.save(function(err, fluffy) {
-  //   if (err) return console.error(err)
-  // })
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
+
+// Import API routes
+const apiRoutes = require('./routes/api-routes')
+app.use('/api', apiRoutes)
 
 async function start() {
   // Init Nuxt.js
@@ -51,3 +41,21 @@ async function start() {
   })
 }
 start()
+
+// const mongoose = require('mongoose')
+// Mongoose Set Up
+// mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error:'))
+// db.once('open', function() {
+//   // Connected
+//   // const kittySchema = new mongoose.Schema({
+//   //   name: String
+//   // })
+//   // const Kitten = mongoose.model('Kitten', kittySchema)
+//   // const silence = new Kitten({ name: 'Silence' })
+//   // const fluffy = new Kitten({ name: 'fluffy' })
+//   // fluffy.save(function(err, fluffy) {
+//   //   if (err) return console.error(err)
+//   // })
+// })
---
+++ server/routes/api-routes.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+const { Router } = require('express')
+const mongoose = require('mongoose')
+const Model = require('../models/models')
+
+const router = Router()
+
+// const users = ['John', 'Winston']
+const db = 'mongodb://127.0.0.1:27017/test'
+
+mongoose.connect(db, (err, res) => {
+  if (err) {
+    console.log('Failed to connected to ' + db)
+  } else {
+    console.log('Connected to ' + db)
+  }
+})
+
+// GET
+router.get('/users', function(req, res) {
+  Model.find({}, (err, users) => {
+    if (err) {
+      res.status(404).send('an error!')
+    } else {
+      res.status(200).send('no error!')
+    }
+  })
+})
+
+// post
+
+router.post('/users', function(req, res) {
+  const User = new Model(req.body)
+  User.save()
+  res.status(201).send(User)
+})
+
+module.exports = router
