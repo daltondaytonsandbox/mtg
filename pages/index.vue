ed6c0b9582b9267aa139d833d9c35d5b8bc19447
Dalton Dayton <daltondayton1@gmail.com>
2019-10-12 20:17:05 -0600

    add form, Post route working

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
     <h1 class="text-center">MTG App</h1>
+
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="name" label="Name" required></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field v-model="age" label="Age" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn class="mr-4" @click="submit">submit</v-btn>
+      </v-container>
+    </v-form>
+    {{ response }}
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      name: '',
+      age: '',
+      response: 'before...'
+    }
+  },
+  methods: {
+    async submit() {
+      console.log('response: ')
+      this.response = await this.$axios.$post('/api/users', {
+        name: this.name,
+        age: this.age
+      })
+      console.log(this.response)
+    }
+  }
+}
 </script>
 
 <style scoped></style>
