69db31d123e963a57009a71b09272a5f957c35fe
Dalton Dayton <daltondayton1@gmail.com>
2019-10-15 21:29:29 -0600

    Move todo list to 'test' page

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,11 @@
 <template>
   <div>
-    <h1 class="text-center">To Do</h1>
-
-    <v-container>
-      <v-form>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="task" label="Task" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-btn class="mr-4" @click.prevent="submit">submit</v-btn>
-        <input @keyup.enter="submit" />
-      </v-form>
-
-      <br />
-      <v-divider />
-      <br />
-      <ol>
-        <li v-for="task in tasks" :key="task.id">
-          <v-container>
-            <v-layout>
-              <div :class="[{ checked: isDone }]">{{ task.task }}</div>
-              &nbsp;
-              <v-icon @click="removeTask(task)">
-                mdi-close
-              </v-icon>
-            </v-layout>
-          </v-container>
-        </li>
-      </ol>
-    </v-container>
+    <h1 class="text-center">Home</h1>
   </div>
 </template>
 
 <script>
-export default {
-  async asyncData({ $axios }) {
-    const tasks = await $axios.$get('/api/tasks')
-    return { tasks }
-  },
-  methods: {
-    async submit() {
-      await this.$axios.$post('/api/tasks', {
-        task: this.task
-      })
-      this.task = ''
-
-      this.tasks = await this.$axios.$get('/api/tasks')
-    },
-    async removeTask(task) {
-      await this.$axios.delete('/api/tasks/' + task._id)
-      this.tasks = await this.$axios.$get('/api/tasks')
-    }
-  }
-}
+export default {}
 </script>
 
-<style scoped>
-.checked {
-  text-decoration: line-through;
-}
-</style>
+<style scoped></style>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,13 +1,52 @@
 <template>
   <div>
-    <!-- <h1 class="text-center">MTG App</h1> -->
+    <v-row>
+      <v-col>
+        <h1>ToDo List</h1>
+        <v-container>
+          <v-form>
+            <v-row>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  v-model="task"
+                  label="Task"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-btn class="mr-4" @click.prevent="submit">submit</v-btn>
+            <input @keyup.enter="submit" />
+          </v-form>
 
-    <ul>
-      <li v-for="user in users" :key="user._id">
-        <v-btn @click="removeUser(user)">X</v-btn>
-        {{ user }}
-      </li>
-    </ul>
+          <br />
+          <v-divider />
+          <br />
+          <ol>
+            <li v-for="task in tasks" :key="task.id">
+              <v-container>
+                <v-layout>
+                  <div :class="[{ checked: isDone }]">{{ task.task }}</div>
+                  &nbsp;
+                  <v-icon @click="removeTask(task)">
+                    mdi-close
+                  </v-icon>
+                </v-layout>
+              </v-container>
+            </li>
+          </ol>
+        </v-container>
+      </v-col>
+      <v-divider class="mx-4" vertical></v-divider>
+      <v-col>
+        <h1>Account List</h1>
+        <ul>
+          <li v-for="user in users" :key="user._id">
+            <v-btn @click="removeUser(user)">X</v-btn>
+            {{ user }}
+          </li>
+        </ul>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -15,12 +54,28 @@
 export default {
   async asyncData({ $axios }) {
     const users = await $axios.$get('/api/users')
-    return { users }
+    const tasks = await $axios.$get('/api/tasks')
+    return {
+      users,
+      tasks
+    }
   },
   methods: {
     async removeUser(user) {
       await this.$axios.delete('/api/users/' + user._id)
       this.users = await this.$axios.$get('/api/users')
+    },
+    async submit() {
+      await this.$axios.$post('/api/tasks', {
+        task: this.task
+      })
+      this.task = ''
+
+      this.tasks = await this.$axios.$get('/api/tasks')
+    },
+    async removeTask(task) {
+      await this.$axios.delete('/api/tasks/' + task._id)
+      this.tasks = await this.$axios.$get('/api/tasks')
     }
   }
 }
