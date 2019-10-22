185854182b245b1106f585944bf6ce8d9a34d370
Dalton Dayton <daltondayton1@gmail.com>
2019-10-21 21:04:00 -0600

    Add Color Wheel component

---
+++ components/ColorWheel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>Color Wheel</h3>
+    <div class="wheelContainer">
+      <img class="colorButton" src="../assets/colors/white.png" alt="white" />
+      <img class="colorButton" src="../assets/colors/blue.png" alt="blue" />
+      <img class="colorButton" src="../assets/colors/green.png" alt="green" />
+      <img class="colorButton" src="../assets/colors/black.png" alt="black" />
+      <img class="colorButton" src="../assets/colors/red.png" alt="red" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: {
+        White: 'Mono White',
+        Blue: 'Mono Blue',
+        Green: 'Mono Green',
+        Black: 'Mono Black',
+        Red: 'Mono Red'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
